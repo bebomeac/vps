c18deb3d55af2048042b67ee60bb29b795c35f19
bebomeac <bebo.t@bk.ru>
2017-04-07 14:40:33 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,24 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "me",
+    "addreplay",
+    "deleall"
+  },
+  info_text = "??¦ Welcome My Dear\n\nTH3BOSS V13 \nFor More Information Subscribe To The Channel @M_2_1 \n https://github.com/moody2020/TH3BOSS\n\n??¦ Dev @xXxDeiQxXx\n ??¦ Dev @S_2_2\n??¦ Dev Bot @llXxDEVxXll_bot\n\n  ??¦ Dev Bot team_bebobot\n??¦ Channel @M_2_1 ",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    369155273,
+    259596128,
+  }
+}
+return _
+end
