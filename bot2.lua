35cba98af96a01a740c9ba1d9f87f75da2a13d4d
bebomeac <bebo.t@bk.ru>
2017-04-10 13:19:57 +0000

    Add files via upload

---
+++ bot2.lua
@@ -0,0 +1,19 @@
+--Begin Ping.lua By @Sajad_Aliraqi
+local datebase = {
+   "BOTBBEBO Is Online\n˝»‹Ê  »Ì»Ê ‘‹€‹«· ?? ",
+  }
+local function th3boss(msg, matches) 
+return datebase[math.random(#datebase)]
+end
+return {
+  patterns = {
+    "^(bot)",
+  "^(bot)",
+˝  "^(»Ê )"
+  },
+  run = th3boss
+}
+
+-- BY @TH3BOSS
+-- CH @llDEV1ll
+-- BY @ll60Kllbot
