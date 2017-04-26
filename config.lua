0a422a3599fbc4c660e19cb9bfef6324484eadfc
bebomeac <bebo.t@bk.ru>
2017-04-26 23:06:28 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,23 +4,19 @@ do local _ = {
   enabled_plugins = {
     "MBC2_BNHR",
     "MBC2_PLUGINS",
-    "MBC2_REPLAY",
-    "MBC2_MGCHS",
+    "MBC2_MGCH",
     "MBC2_TOOLS",
     "MBC2_ME",
     "MBC2_SUPER",
-    "MBC2_ADD_REPLAY"
-  },
-  info_text = "المطورين❤️😍\n\n 1:@xXxDe_iQxXx\n\n بوت تواصل☝🏻\n\n @llXxDEVxXll_bot\n\n 2:  @S_2_2\n\n بوت تواصل☝🏻\n\n @team_bebobot\n\n 3:@Cr3rC\n\n بوت تواصل☝🏻\n\n @CD_CD_Ali_bot\n\n قناة البوت 👇🏻\n\n @M_2_1\n\n",
+    },
+  info_text = "آ̯͡ل̯͡م̯͡ط̯͡ۆ̯͡ر̯͡ي̯͡ن̯͡ ❤️😍\n (̲̅d̲̅)(̲̅e̲̅)(̲̅v̲̅)1:@xXxDe_iQxXx\n\n  ﯝتہ تہﯝٳصًلہ ☝🏻\n\n ᴰᴱᵛᴮᴼᵀ1 @llXxDEVxXll_bot\n\n (̲̅d̲̅)(̲̅e̲̅)(̲̅v̲̅)2:@S_2_2\n\n ﯝتہ تہﯝٳصًلہ ☝🏻\n\n ᴰᴱᵛᴮᴼᵀ2 @team_bebobot\n\n ᴮᴼᵀ ᶜᴴᴬᴺᴺᴱᴸ👇🏻\n  @M_2_1\n",
   moderation = {
     data = "./data/moderation.json"
   },
   sudo_users = {
-    343739716,
     369155273,
     259596128,
-    272879264,
-    60210262
+    343739716,
   }
 }
 return _
